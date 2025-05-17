93f32c83698d2ae10e9dc00065464c1380a1fd05
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-05-17 13:44:47 +0330

    feat add lower and uper

--- sigils_practice/lib/sigils_practice.ex
+++ sigils_practice/lib/sigils_practice.ex
@@ -1,17 +1,4 @@
 defmodule SigilsPractice do
-  @moduledoc """
-  Documentation for `SigilsPractice`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SigilsPractice.hello()
-      :world
-
-  """
   def string_examples do
     ~s(این یک رشته است با "دابل کوتیشن")
     # معادل: "این یک رشته است با \"دابل کوتیشن\""
@@ -28,7 +15,6 @@ defmodule SigilsPractice do
 
     ~w(سلام 42 true)a
     # [:سلام, 42, true]
-
   end
 
   def regex_example do
@@ -44,14 +30,70 @@ defmodule SigilsPractice do
 
   defmodule MySigils do
     def sigil_u(string, []), do: String.upcase(string)
+    def sigil_l(string, []), do: String.downcase(string)
+  end
+
+  def custom_sigils do
+    import MySigils
+    [
+      ~u(hello),  # "HELLO"
+      ~l(WORLD)   # "world"
+    ]
   end
   # استفاده:
   # import MySigils
   # ~u(hello)  # "HELLO"
 
-  def date_example do
+  def s_example do
+    name = "الی"
+    # حروف کوچک - interpolation فعال
+    # => "سلام الی"
+    ~s(سلام #{name})
+    # حروف بزرگ - interpolation غیرفعال
+    # => "سلام \#{name}"
+    ~S(سلام #{name})
+  end
+
+  def c_example do
+    # حروف کوچک - کاراکترهای خاص تفسیر می‌شوند
+    ~c(خط اول\nخط دوم)
+    # => 'خط اول\nخط دوم' (کاراکتر \n تفسیر می‌شود)
+
+    # حروف بزرگ - نمایش خام
+    ~C(خط اول\nخط دوم)
+
+    # => 'خط اول\\nخط دوم' (کاراکتر \n به صورت متن نمایش داده می‌شود)
+  end
+
+  def r_example do
+    prefix = "ال"
+    # حروف کوچک - interpolation فعال
+    ~r/#{prefix}یکسیر/
+    # => ~r/الیکسیر/
+
+    # حروف بزرگ - interpolation غیرفعال
+    ~R/#{prefix}یکسیر/
+    # => ~r/\#{prefix}یکسیر/
+  end
+
+  def w_example do
+    count = 2
+
+    # حروف کوچک - interpolation فعال
+    ~w(آیتم#{count} جدید)
+    # => ["آیتم2", "جدید"]
+
+    # حروف بزرگ - interpolation غیرفعال
+    ~W(آیتم#{count} جدید)
+    # => ["آیتم\#{count}", "جدید"]
+  end
+
+  def d_example do
+    # فقط حروف بزرگ برای تاریخ وجود دارد
+    # معتبر
     ~D[2023-12-25]
-    # %Date{year: 2023, month: 12, day: 25}
+    # نامعتبر
+    ~d[2023-12-25]
   end
 
   def hello do
